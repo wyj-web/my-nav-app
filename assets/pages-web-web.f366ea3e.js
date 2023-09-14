2e537a8a2a549c1f907322175e4f7570d5083eb4
wyj_github <www15294729715@outlook.com>
2023-09-14 18:13:33 +0800

    提示

---
+++ assets/pages-web-web.f366ea3e.js
@@ -0,0 +1,1 @@
+import{V as s,o as r,c as e,L as t,W as a}from"./index-38084ba6.js";const n={__name:"web",setup(n){let u=s();return(s,n)=>{const o=a;return r(),e(o,{src:t(u).query.url},null,8,["src"])}}};export{n as default};
