c147d34f1717d523ef8229f8b04c6292e5e44901
wyj_github <www15294729715@outlook.com>
2023-09-15 20:57:35 +0800

    页码

---
+++ assets/pages-web-web.1456a3eb.js
@@ -0,0 +1,1 @@
+import{a7 as s,o as r,c as a,E as e,a8 as t}from"./index-89f1d1fc.js";const n={__name:"web",setup(n){let u=s();return(s,n)=>{const o=t;return r(),a(o,{src:e(u).query.url},null,8,["src"])}}};export{n as default};
