11cddccc3c18a5493b7742dde578d000b242418e
wyj_github <www15294729715@outlook.com>
2023-09-15 13:41:38 +0800

    接口

---
+++ assets/pages-web-web.81b24fc2.js
@@ -0,0 +1,1 @@
+import{a6 as s,o as r,c as a,M as e,a7 as t}from"./index-3ce175bf.js";const n={__name:"web",setup(n){let u=s();return(s,n)=>{const o=t;return r(),a(o,{src:e(u).query.url},null,8,["src"])}}};export{n as default};
